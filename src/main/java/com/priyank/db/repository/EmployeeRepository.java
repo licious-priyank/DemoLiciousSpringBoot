35605d6ad86c368232ac22e988a62263bbe09521
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-07-28 17:13:36 +0530

    Add files via upload

--- src/main/java/com/priyank/db/repository/EmployeeRepository.java
+++ src/main/java/com/priyank/db/repository/EmployeeRepository.java
@@ -1,1 +1,11 @@
+package com.priyank.db.repository;
 
+import com.priyank.db.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Creating a repository to interact with tables from the database.
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
+
+//Now we can use JpaRepository’s methods: save(), findOne(),
+// findById(), findAll(), count(), delete(), deleteById()… without implementing these methods.
