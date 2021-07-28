5babf0b9698d901f610f6ba3820c4a338f580d5b
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-07-28 17:10:44 +0530

    Add files via upload

--- src/main/java/com/priyank/db/exception/ControllerException.java
+++ src/main/java/com/priyank/db/exception/ControllerException.java
@@ -1,1 +1,37 @@
+package com.priyank.db.exception;
+
+import org.springframework.stereotype.Component;
+
+@Component //@Component Spring will: Scan our application for classes annotated with @Component.
+// Instantiate them and inject any specified dependencies into them.
+public class ControllerException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    private String errorCode;
+    private String errorMessage;
+    public String getErrorCode() {
+        return errorCode;
+    }
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+    public ControllerException(String errorCode, String errorMessage) {
+        super();
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public ControllerException() {
+
+    }
+}
 
