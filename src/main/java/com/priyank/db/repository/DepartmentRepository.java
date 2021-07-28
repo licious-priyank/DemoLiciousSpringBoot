2bc7df6eb993e5d2b584e7c4fe627e91d349f9eb
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-07-28 17:12:54 +0530

    Add files via upload

--- src/main/java/com/priyank/db/repository/DepartmentRepository.java
+++ src/main/java/com/priyank/db/repository/DepartmentRepository.java
@@ -1,1 +1,7 @@
+package com.priyank.db.repository;
 
+import com.priyank.db.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+}
