6458091bfcbdeef1beb84e2cee15d84cf3dd1270
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-08-02 11:34:39 +0530

    Create DepartmentDDL.sql

---
+++ script/DepartmentDDL.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `department` (
+  `dept_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `is_active` tinyint DEFAULT '1',
+  PRIMARY KEY (`dept_id`),
+  UNIQUE KEY `Name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
