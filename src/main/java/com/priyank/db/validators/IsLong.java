f94d93fbf8ea20ce2bf032a1638b2ae0ed27ed1e
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-08-09 10:26:06 +0530

    Update IsLong.java

--- src/main/java/com/priyank/db/validators/IsLong.java
+++ src/main/java/com/priyank/db/validators/IsLong.java
@@ -1,1 +1,21 @@
 
+package com.priyank.db.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = LongValidator.class)
+@Target({FIELD, PARAMETER})
+@Retention(RUNTIME)
+public @interface IsLong {
+    String message() default "Should be a Long Integer";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
