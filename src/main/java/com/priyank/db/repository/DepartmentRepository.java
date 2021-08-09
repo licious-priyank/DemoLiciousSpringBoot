7e56c938e3e6df953571f5fd6a2dc5bb8ecbb937
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-08-09 09:58:54 +0530

    Update DepartmentRepository.java

--- src/main/java/com/priyank/db/repository/DepartmentRepository.java
+++ src/main/java/com/priyank/db/repository/DepartmentRepository.java
@@ -1,7 +1,14 @@
 package com.priyank.db.repository;
-
 import com.priyank.db.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Modifying;
+import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+    public List<Department> findByName(@Param("name") String name);
+    public Optional<Department> findById(@Param("dept_id") Integer dept_id);
+    @Modifying
+    public void deleteById(@Param("Id") Integer Id);
 }
