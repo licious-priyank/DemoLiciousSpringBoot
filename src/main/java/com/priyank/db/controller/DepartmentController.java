66b3c0456019b937b99cf88bc6ae3d55b8649fda
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-08-09 09:57:38 +0530

    Update DepartmentController.java

--- src/main/java/com/priyank/db/controller/DepartmentController.java
+++ src/main/java/com/priyank/db/controller/DepartmentController.java
@@ -1,22 +1,20 @@
 package com.priyank.db.controller;
 
-import java.util.List;
-
+import com.priyank.db.model.Department;
+import com.priyank.db.repository.DepartmentRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
-import com.priyank.db.model.Department;
-
-import com.priyank.db.service.DepartmentService;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController //@RestController annotation is used to define a controller
 // and to indicate that the return value of the methods should be be bound to the web response body.
@@ -25,47 +23,44 @@ public class DepartmentController {
     static final Logger logger  = LogManager.getLogger(DepartmentController.class.getName());
 
     @Autowired
-    private DepartmentService departmentService;
+    DepartmentRepository departmentRepository;
 
-    // displaying list of all department
-    @GetMapping("/departments")
-    public List<Department> getAllDepartment(){
-        return departmentService.getAllDepartments();
+    @GetMapping(value = "/department/all")
+    public List<Department> getAll() {
+        return departmentRepository.findAll();
     }
 
-    // displaying department by id
-    @GetMapping("/departments/{id}")
-    public Department getDepartment(@PathVariable int id){
-       return departmentService.getDepartment(id);
+    @PostMapping(value = "/department/insert")
+    public ResponseEntity<@Valid Department> persist(@Valid @RequestBody final Department department) {
+        departmentRepository.save(department);
+        return ResponseEntity.ok(department);
     }
 
-    // inserting department
-    @PostMapping("/departments")
-    public void addDepartment(@RequestBody Department department){
-        departmentService.addDepartment(department);
+    @GetMapping("/department/findbyname")
+    public List<Department> fetchDataByName(@Valid @RequestParam("name") String name){
+        return departmentRepository.findByName(name);
     }
 
-    //updating department by id
-    @PutMapping("/departments/{id}")
-    public void updateDepartment(@RequestBody Department d, @PathVariable int id){
-        departmentService.updateDepartment(d, id);
+
+    @DeleteMapping("/department/delete")
+    public ResponseEntity<String> deleteDepartment(@RequestParam("dept_id") Integer dept_id){
+        try {
+            departmentRepository.deleteById(dept_id);
+            return ResponseEntity.ok("Success");
+        } catch(Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    // deleting all department
-    @DeleteMapping("/departments")
-    public void deleteAllDepartments(){
-        departmentService.deleteAllDepartment();
-    }
-
-    // deleting department by id
-    @DeleteMapping("departments/{id}")
-    public void deleteDepartmentByID(@RequestBody Department d, @PathVariable int id){
-        departmentService.deleteDepartmentByID(id);
-    }
-
-      //updating status of IsActive using patch
-    @PatchMapping("departments/{id}")
-    public void patchDepartmentByID(@RequestBody Department d, @PathVariable int id) {
-       departmentService.patchDepartment(d, id);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
