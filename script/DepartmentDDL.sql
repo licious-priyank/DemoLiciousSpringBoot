56c9d03f3b203b71c9cfb2554f9717d9e31275e0
licious-priyank <87962386+licious-priyank@users.noreply.github.com>
2021-08-10 10:34:34 +0530

    Update DepartmentDDL.sql

--- script/DepartmentDDL.sql
+++ script/DepartmentDDL.sql
@@ -2,7 +2,7 @@ CREATE TABLE `department` (
   `dept_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `is_active` tinyint DEFAULT '1',
+  `is_active` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`dept_id`),
   UNIQUE KEY `Name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
